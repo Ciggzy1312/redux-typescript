e052285c36dddd6922dd8724cd04e3d17fe777b2
Deepayan <deepayanmukherjee1312@gmail.com>
2022-01-19 08:27:36 +0530

    Added actions and reducer

---
+++ src/redux/actions/actions.ts
@@ -0,0 +1,17 @@
+import { ActionType } from "./action-types"
+
+interface SearchPackageAction {
+    type : ActionType.SEARCH_PACKAGES
+}
+
+interface SearchPackageSuccessAction {
+    type : ActionType.SEARCH_PACKAGES_SUCCESS;
+    payload : string[]
+}
+
+interface SearchPackageFailureAction {
+    type : ActionType.SEARCH_PACKAGES_FAILURE;
+    payload : string
+}
+
+export type Action = SearchPackageAction | SearchPackageSuccessAction | SearchPackageFailureAction
---
+++ src/redux/reducers/packageReducers.ts
@@ -0,0 +1,38 @@
+import { ActionType } from "../actions/action-types"
+import { Action } from "../actions/actions"
+
+interface PackageState {
+    data : string[];
+    loading : boolean,
+    error : string | null
+}
+
+const reducer = (state : PackageState, action : Action) : PackageState=>{
+    switch(action.type){
+        case ActionType.SEARCH_PACKAGES :
+            return {
+                data : [],
+                loading: true,
+                error : null
+            }
+
+            case ActionType.SEARCH_PACKAGES_SUCCESS :
+                return {
+                    data : action.payload,
+                    loading: false,
+                    error : null
+                }
+
+            case ActionType.SEARCH_PACKAGES_FAILURE :
+                return {
+                    data : [],
+                    loading: false,
+                    error : action.payload
+                }
+
+            default:
+                return state
+    }
+}
+
+export default reducer
