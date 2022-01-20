4a3e741c9bb256081c170b62dc45e98d1d2acca4
Deepayan <deepayanmukherjee1312@gmail.com>
2022-01-20 16:13:46 +0530

    Added useTypedSelector hook to select returned state

--- src/components/PackagesList.tsx
+++ src/components/PackagesList.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelectors';
 import { useActions } from '../hooks/useActions';
 
 const PackagesList: React.FC = () => {
 
   const [term, setTerm] = useState("")
   const { searchPackage } = useActions()
+  const { data, loading, error } = useTypedSelector((state)=> state.packages)
 
   const handleSubmit = (e : React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
